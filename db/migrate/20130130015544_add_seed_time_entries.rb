b3420f37996b5767cb55b5af356df908837c5607
Matt <matt@randomgram.com>
2013-02-23 00:34:24 +0000

    update migration

--- db/migrate/20130130015544_add_seed_time_entries.rb
+++ db/migrate/20130130015544_add_seed_time_entries.rb
@@ -1,11 +1,11 @@
 class AddSeedTimeEntries < ActiveRecord::Migration
   def up
-User.create(:id => 1, :email => "mscullerton@gmail.com", :encrypted_password => "$2a$10$UlBGHo1DCRQ4319.hGy0eeXhfksa1Wrv9/mH96V2xacK7pOJvimwW", :sign_in_count => 4, :current_sign_in_at => Wed, 30 Jan 2013 23:13:17 UTC +00:00, :last_sign_in_at => Wed, 30 Jan 2013 18:42:14 UTC +00:00, :current_sign_in_ip => "127.0.0.1", :last_sign_in_ip => "127.0.0.1")
-    Account.create(:id => 1, :name => "F#", :contact_name => "Pete Jimison", :contact_email => "ap@efsharp.com", :rate => 50, :creation_date => Wed, 30 Jan 2013 03:58:00 UTC +00:00)
-    Account.create(:id => 2, :name => "EMN8", :contact_name => "James Pinpin", :contact_email => "jpinpin@emn8.com", :rate => 75, :creation_date => Wed, 30 Jan 2013 04:03:00 UTC +00:00)
-    Invoice.create(:id => 2, :issued => true, :issued_date => Thu, 16 Aug 2012 00:24:00 UTC +00:00, :user_id => 1, :account_id => 1)
-    Invoice.create(:id => 3, :issued => true, :issued_date => Wed, 19 Sep 2012 00:00:00 UTC +00:00, :user_id => 1, :account_id => 1)
-    Invoice.create(:id => 4, :issued => true, :issued_date => Sun, 16 Dec 2012 00:00:00 UTC +00:00, :user_id => 1, :account_id => 1)
+    User.create(:id => 1, :email => "mscullerton@gmail.com", :encrypted_password => "$2a$10$UlBGHo1DCRQ4319.hGy0eeXhfksa1Wrv9/mH96V2xacK7pOJvimwW", :sign_in_count => 4, :current_sign_in_at => DateTime.parse("30/01/2013 23:13"), :last_sign_in_at => DateTime.parse("30/01/2013 23:13"), :current_sign_in_ip => "127.0.0.1", :last_sign_in_ip => "127.0.0.1")
+    Account.create(:id => 1, :name => "F#", :contact_name => "Pete Jimison", :contact_email => "ap@efsharp.com", :rate => 50, :creation_date => DateTime.parse("30/01/2013 23:13"))
+    Account.create(:id => 2, :name => "EMN8", :contact_name => "James Pinpin", :contact_email => "jpinpin@emn8.com", :rate => 75, :creation_date => DateTime.parse("30/01/2013 23:13"))
+    Invoice.create(:id => 2, :issued => true, :issued_date => DateTime.parse("16/08/2012 00:00"), :user_id => 1, :account_id => 1)
+    Invoice.create(:id => 3, :issued => true, :issued_date => DateTime.parse("19/09/2012 00:00"), :user_id => 1, :account_id => 1)
+    Invoice.create(:id => 4, :issued => true, :issued_date => DateTime.parse("16/12/2012 00:00"), :user_id => 1, :account_id => 1)
     TimeEntry.create({:date => DateTime.parse("28/06/2012 00:00"), :hours => ".5", :description => "Conversation with Peter", :account => Account.find(1), :user => User.find(1), :invoice => Invoice.find(2)})
     TimeEntry.create({:date => DateTime.parse("29/06/2012 00:00"), :hours => "1", :description => "Putting together Documentation for Peter", :account => Account.find(1), :user => User.find(1), :invoice => Invoice.find(2)})
     TimeEntry.create({:date => DateTime.parse("06/07/2012 00:00"), :hours => "1", :description => "Debugging Workflow issue for Peter", :account => Account.find(1), :user => User.find(1), :invoice => Invoice.find(2)})
