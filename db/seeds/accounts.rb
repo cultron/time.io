49f9aca91ff1d9895cb8e3831b4e2011c9f2858e
Matt Cullerton <Matthew.Cullerton@MedImpact.com>
2013-05-14 09:19:21 -0700

    clean up

--- db/seeds/accounts.rb
+++
@@ -1,2 +0,0 @@
-Account.create(:id => 1, :name => "F#", :contact_name => "Pete Jimison", :contact_email => "ap@efsharp.com", :rate => 50, :creation_date => Wed, 30 Jan 2013 03:58:00 UTC +00:00)
-Account.create(:id => 2, :name => "EMN8", :contact_name => "James Pinpin", :contact_email => "jpinpin@emn8.com", :rate => 75, :creation_date => Wed, 30 Jan 2013 04:03:00 UTC +00:00)
--- db/seeds/invoices.rb
+++
@@ -1,3 +0,0 @@
-Invoice.create(:id => 2, :issued => true, :issued_date => Thu, 16 Aug 2012 00:24:00 UTC +00:00, :user_id => 1, :account_id => 1)
-Invoice.create(:id => 3, :issued => true, :issued_date => Wed, 19 Sep 2012 00:00:00 UTC +00:00, :user_id => 1, :account_id => 1)
-Invoice.create(:id => 4, :issued => true, :issued_date => Sun, 16 Dec 2012 00:00:00 UTC +00:00, :user_id => 1, :account_id => 1)
